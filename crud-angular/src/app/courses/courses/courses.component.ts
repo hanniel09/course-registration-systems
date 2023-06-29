930a816770acb73c3396580e30652794d9e6bd4c
hanniel09 <hannielvieira1227@gmail.com>
2023-06-29 17:41:05 -0300

    feature

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 
 import { Course } from '../models/course';
 import { CoursesService } from './../services/courses.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,11 +14,14 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog){
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+    ){
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
@@ -33,5 +37,11 @@ export class CoursesComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
+  ngOnInit(): void {
+     /* TODO document why this method 'ngOnInit' is empty */
+    }
+
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
 }
