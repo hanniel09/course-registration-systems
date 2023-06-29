ecd8074225491cb992c1915649824a8d2e83f036
hanniel09 <hannielvieira1227@gmail.com>
2023-06-29 11:00:05 -0300

    feature

--- crud-spring/src/main/java/com/hanniel/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.hanniel.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hanniel.crudspring.model.Course;
+import com.hanniel.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
---
+++ crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.hanniel.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hanniel.crudspring.model.Course;
+import com.hanniel.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+  @Autowired
+  private final CourseRepository courseRepository;
+
+  @GetMapping
+  public List<Course> list() {
+    return courseRepository.findAll();
+  }
+}
