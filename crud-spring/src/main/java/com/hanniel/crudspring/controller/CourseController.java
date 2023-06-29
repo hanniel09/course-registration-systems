657e4cf0e406a9ffd3bf1e4fd42248086e4f2da0
hanniel09 <hannielvieira1227@gmail.com>
2023-06-29 14:16:02 -0300

    feature

--- crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanniel.crudspring.model.Course;
@@ -21,7 +22,7 @@ public class CourseController {
   private final CourseRepository courseRepository;
 
   @GetMapping
-  public List<Course> list() {
+  public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
   }
 }
--- crud-spring/src/main/java/com/hanniel/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.hanniel.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import lombok.Data;
 public class Course {  
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonProperty("_id")
   private Long id;
 
   @Column(length = 200, nullable = false)
