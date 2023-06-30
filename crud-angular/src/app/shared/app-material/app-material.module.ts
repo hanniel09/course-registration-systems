9fc9d13f017c7352ec2a300a765f704a49bf8152
hanniel09 <hannielvieira1227@gmail.com>
2023-06-30 16:59:21 -0300

    feature

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
-  
+
+
+
 
 @NgModule({
   exports:[
@@ -19,7 +23,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   MatProgressSpinnerModule,
   MatDialogModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
   ]
 })
 export class AppMaterialModule { }
