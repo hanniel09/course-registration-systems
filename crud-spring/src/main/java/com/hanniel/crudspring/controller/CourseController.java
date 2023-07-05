1075416729ddc6d5cc900a60035ed49c2724ea18
hanniel09 <hannielvieira1227@gmail.com>
2023-07-05 20:46:42 -0300

    feature

--- crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,16 @@ public class CourseController {
                 recordFound.setCategory(course.getCategory());
                 Course updated = courseRepository.save(recordFound);
                 return ResponseEntity.ok().body(updated);
+              })
+              .orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id){
+      return courseRepository.findById(id)
+              .map(recordFound -> {
+                courseRepository.deleteById(id);
+                return ResponseEntity.noContent().<Void>build();
               })
               .orElse(ResponseEntity.notFound().build());
   }
