2e9e66ab37a9dd5b8ad54dcd2f1db450525617f6
hanniel09 <hannielvieira1227@gmail.com>
2023-07-08 11:52:02 -0300

    feature

--- crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
@@ -2,7 +2,6 @@ package com.hanniel.crudspring.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -18,61 +17,58 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanniel.crudspring.model.Course;
-import com.hanniel.crudspring.repository.CourseRepository;
+import com.hanniel.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
 public class CourseController {
 
-  @Autowired
-  private final CourseRepository courseRepository;
+    private final CourseService courseService;
 
-  @GetMapping
-  public @ResponseBody List<Course> list() {
-    return courseRepository.findAll();
-  }
+    public CourseController(CourseService courseService){
+      this.courseService = courseService;
+    }
+    
 
-  @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id){
-    return courseRepository.findById(id)
-          .map(recordFound -> ResponseEntity.ok().body(recordFound))
-          .orElse(ResponseEntity.notFound().build());
-  }
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+      return courseService.list();
+    }
 
-  @PostMapping
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public Course create(@RequestBody @Valid Course course){
-    return courseRepository.save(course);
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id){
+      return courseService.findById(id)
+            .map(recordFound -> ResponseEntity.ok().body(recordFound))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody @Valid Course course){
+      return courseService.create(course);
+    }
 
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, 
-          @RequestBody Course course){
-    return courseRepository.findById(id)
-              .map(recordFound -> {
-                recordFound.setName(course.getName());
-                recordFound.setCategory(course.getCategory());
-                Course updated = courseRepository.save(recordFound);
-                return ResponseEntity.ok().body(updated);
-              })
-              .orElse(ResponseEntity.notFound().build());
-  }
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, 
+            @RequestBody Course course){
+      return courseService.update(id, course)
+                .map(recordFound -> {
+                  return ResponseEntity.ok().body(recordFound);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
-      return courseRepository.findById(id)
-              .map(recordFound -> {
-                courseRepository.deleteById(id);
-                return ResponseEntity.noContent().<Void>build();
-              })
-              .orElse(ResponseEntity.notFound().build());
-  }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
+        if (courseService.delete(id)) {
+          return ResponseEntity.noContent().<Void>build();
+       }
+       return ResponseEntity.notFound().build();
+      }
 }
