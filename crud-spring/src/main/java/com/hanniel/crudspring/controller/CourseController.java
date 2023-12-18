918aae7a7a6dd1feaf85df17339bc18e329a6a5c
hanniel09 <hannielvieira1227@gmail.com>
2023-12-18 18:03:25 -0300

    feat: Page Courses

--- crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/controller/CourseController.java
@@ -2,17 +2,12 @@ package com.hanniel.crudspring.controller;
 
 import java.util.List;
 
+import com.hanniel.crudspring.dto.CoursePageDTO;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hanniel.crudspring.dto.CourseDTO;
 import com.hanniel.crudspring.service.CourseService;
@@ -33,12 +28,17 @@ public class CourseController {
       this.courseService = courseService;
     
     }
-    
 
     @GetMapping
-    public List<CourseDTO> list() {
-      return courseService.list();
+    public CoursePageDTO list(@RequestParam(defaultValue = "0") @PositiveOrZero int page,
+                              @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize) {
+        return courseService.list(page, pageSize);
     }
+
+//    @GetMapping
+//    public List<CourseDTO> list() {
+//      return courseService.list();
+//    }
 
     @GetMapping("/{id}")
     public CourseDTO findById(@PathVariable @NotNull @Positive Long id){
