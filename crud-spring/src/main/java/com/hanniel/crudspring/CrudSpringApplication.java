bdfab30193b752db19890bc7224c669b0e6783fe
hanniel09 <hannielvieira1227@gmail.com>
2023-12-18 18:09:59 -0300

    feat: Page Courses

--- crud-spring/src/main/java/com/hanniel/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/hanniel/crudspring/CrudSpringApplication.java
@@ -22,24 +22,27 @@ public class CrudSpringApplication {
 		return args -> {
 			courseRepository.deleteAll();
 
-			Course c = new Course();
-			c.setName("Angular com Spring");
-			c.setCategory(Category.FRONT_END);
+			for(int i = 0; i < 20; i++) {
 
-			Lesson l = new Lesson();
-			l.setName("Introdução");
-			l.setYoutubeUrl("watch?v=N1");
-			l.setCourse(c);
-			c.getLessons().add(l);
+				Course c = new Course();
+				c.setName("Angular com Spring" + i);
+				c.setCategory(Category.FRONT_END);
 
-			Lesson l1 = new Lesson();
-			l1.setName("Angular");
-			l1.setYoutubeUrl("watch?v=N1");
-			l1.setCourse(c);
-			c.getLessons().add(l1);
-			
+				Lesson l = new Lesson();
+				l.setName("Introdução");
+				l.setYoutubeUrl("watch?v=N1");
+				l.setCourse(c);
+				c.getLessons().add(l);
 
-			courseRepository.save(c);
+				Lesson l1 = new Lesson();
+				l1.setName("Angular");
+				l1.setYoutubeUrl("watch?v=N1");
+				l1.setCourse(c);
+				c.getLessons().add(l1);
+
+
+				courseRepository.save(c);
+			}
 		};
 	}
 }
