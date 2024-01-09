8303e3ad7bc0c7bf9345449d951f039ed77126df
hanniel09 <hannielvieira1227@gmail.com>
2024-01-09 18:22:38 -0300

    feat: edit routes

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,22 +1,24 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { CourseResolver } from './app/courses/guards/course.resolver';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {withInterceptorsFromDi, provideHttpClient} from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
+import {CourseResolver} from './app/courses/guards/course.resolver';
+import {PreloadAllModules, provideRouter, withPreloading} from "@angular/router";
+import {APP_ROUTES} from "./app/app.routes";
 
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
-        CourseResolver,
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
+  providers: [
+    importProvidersFrom(BrowserModule, MatToolbarModule),
+    CourseResolver,
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
+  ]
 })
   .catch(err => console.error(err));
